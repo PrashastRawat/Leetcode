85264ff0293a9efb90ef51aa01cea474e51ddbb5
PrashastRawat <prashastrawat123@gmail.com>
2026-08-19 14:22:12 +0530

    Time: 14 ms (21.60%) | Memory: 17.2 MB (9.32%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -1,19 +1,17 @@
 class Solution(object):
     def merge(self, intervals):
         intervals.sort()
-
         res = []
         res.append(intervals[0])
-
         for i in range(1, len(intervals)):
-            curstart = intervals[i][0]
-            curend = intervals[i][1]
+            curs = intervals[i][0]
+            cure = intervals[i][1]
 
-            lstart = res[-1][0]
-            lend = res[-1][1]
-            if curstart<=lend:
-                if curend>lend:
-                    res[-1][1]=curend
+            laste = res[-1][1]
+            lasts = res[-1][0]
+            if curs<=laste:
+                if cure>laste:
+                    res[-1][1]=cure
             else:
-                res.append([curstart, curend])
+                res.append([curs,cure])
         return res
